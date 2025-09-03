b87c46359c3b45f1c4278bf7da4d8b3cb69d4a93
Iing Muslihin <itjenkemenagai@gmail.com>
2025-09-03 16:13:01 +0700

    Fix: Perbaikan HTML dan rilis v1.0.5

--- preload.js
+++ preload.js
@@ -1,10 +1,15 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-// Mengekspos API yang aman ke proses renderer (index.html)
 contextBridge.exposeInMainWorld('electronAPI', {
-  // Menerima event dari main process dan meneruskannya ke window
+  // Dari Renderer ke Main
+  restartApp: () => ipcRenderer.send('restart_app'),
+
+  // Dari Main ke Renderer
   onUpdateAvailable: (callback) => ipcRenderer.on('update_available', callback),
   onUpdateDownloaded: (callback) => ipcRenderer.on('update_downloaded', callback),
-  // Mengirim event dari renderer ke main process
-  restartApp: () => ipcRenderer.send('restart_app'),
+  
+  // Saluran baru untuk umpan balik
+  onUpdateChecking: (callback) => ipcRenderer.on('update-checking', callback),
+  onUpdateNotAvailable: (callback) => ipcRenderer.on('update-not-available', callback),
+  onUpdateError: (callback) => ipcRenderer.on('update-error', (_event, error) => callback(error)),
 });
